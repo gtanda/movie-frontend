62aa0d1a48814b8392ffd7969da1fb3389e32662
Govind <govind_tanda1@hotmail.com>
2022-05-28 19:50:21 -0700

    added react carousel for videocards

--- src/components/VideoDisplay.js
+++ src/components/VideoDisplay.js
@@ -1,11 +1,38 @@
 import MovieCard from "./MovieCard";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import {Carousel} from 'react-responsive-carousel';
-
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3,
+        slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        slidesToSlide: 1 // optional, default to 1.
+    }
+};
 const VideoDisplay = ({videos, user, onHover, onLeave}) => {
     return (
         <div>
-            <Carousel infiniteLoop={true}>
+            <Carousel swipeable={false}
+                      draggable={false}
+                      showDots={true}
+                      responsive={responsive}
+                      ssr={true} // means to render carousel on server-side.
+                      infinite={true}
+                      keyBoardControl={true}
+                      customTransition="all .5"
+                      containerClass="carousel-container"
+                      removeArrowOnDeviceType={["tablet", "mobile"]}
+                      dotListClass="custom-dot-list-style"
+                      itemClass="carousel-item-padding-40-px">
             {videos
                 ? videos.map((movie) => {
                     return (
