8f41eef31e09972cb3b0cd7e184ab7577b0e529f
gtanda <govind_tanda1@hotmail.com>
2022-07-27 17:20:33 -0700

    Fixing package.json and video display

--- src/components/VideoDisplay.js
+++ src/components/VideoDisplay.js
@@ -1,5 +1,4 @@
 import MovieCard from "./MovieCard";
-<<<<<<< HEAD
 import "react-multi-carousel/lib/styles.css";
 import Carousel from 'react-multi-carousel';
 const responsive = {
@@ -7,15 +6,6 @@ const responsive = {
         breakpoint: { max: 3000, min: 1024 },
         items: 5,
         slidesToSlide: 2 // optional, default to 1.
-=======
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-const responsive = {
-    desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 3,
-        slidesToSlide: 3 // optional, default to 1.
->>>>>>> 62aa0d1a48814b8392ffd7969da1fb3389e32662
     },
     tablet: {
         breakpoint: { max: 1024, min: 464 },
@@ -28,29 +18,11 @@ const responsive = {
         slidesToSlide: 1 // optional, default to 1.
     }
 };
-<<<<<<< HEAD
 
 const VideoDisplay = ({videos, user, onHover, onLeave}) => {
     return (
         <div>
             <Carousel infinite={true} responsive={responsive}>
-=======
-const VideoDisplay = ({videos, user, onHover, onLeave}) => {
-    return (
-        <div>
-            <Carousel swipeable={false}
-                      draggable={false}
-                      showDots={true}
-                      responsive={responsive}
-                      ssr={true} // means to render carousel on server-side.
-                      infinite={true}
-                      keyBoardControl={true}
-                      customTransition="all .5"
-                      containerClass="carousel-container"
-                      removeArrowOnDeviceType={["tablet", "mobile"]}
-                      dotListClass="custom-dot-list-style"
-                      itemClass="carousel-item-padding-40-px">
->>>>>>> 62aa0d1a48814b8392ffd7969da1fb3389e32662
             {videos
                 ? videos.map((movie) => {
                     return (
