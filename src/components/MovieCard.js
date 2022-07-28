00e7c0ad51b8136d2f5ce2b5a301fe64c82293a2
gtanda <govind_tanda1@hotmail.com>
2022-07-27 17:16:23 -0700

    Fixing carousel

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -49,7 +49,6 @@ const MovieCard = ({ user,trendingData, onHover, onLeave }) => {
         )
     }
 
-
     return (
         <>
             <Modal
--- src/components/VideoDisplay.js
+++ src/components/VideoDisplay.js
@@ -1,11 +1,28 @@
 import MovieCard from "./MovieCard";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import {Carousel} from 'react-responsive-carousel';
+import "react-multi-carousel/lib/styles.css";
+import Carousel from 'react-multi-carousel';
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 5,
+        slidesToSlide: 2 // optional, default to 1.
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        slidesToSlide: 1 // optional, default to 1.
+    }
+};
 
 const VideoDisplay = ({videos, user, onHover, onLeave}) => {
     return (
         <div>
-            <Carousel infiniteLoop={true}>
+            <Carousel infinite={true} responsive={responsive}>
             {videos
                 ? videos.map((movie) => {
                     return (
