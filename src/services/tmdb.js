9dc70b1a81be1315fe6f32b48f4f2284df32e624
gtanda <govind_tanda1@hotmail.com>
2022-09-14 18:46:59 -0700

    adding playwright e2e

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const baseUrl = '/api/tmdb';
 
 const getTrending = async (type) => {
-    const request = await axios.get(`${baseUrl}/trending`, {params: {searchType: type}});
+    const request = await axios.get(`${baseUrl}`, {params: {searchType: type}});
     return request.data;
 };
 
