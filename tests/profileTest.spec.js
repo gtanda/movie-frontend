bfc2e6650f93cee8f32b2a4284a64afc4e2ede43
gtanda <govind_tanda1@hotmail.com>
2022-09-15 19:33:28 -0700

    fix profile name change test

--- tests/profileTest.spec.js
+++ tests/profileTest.spec.js
@@ -5,19 +5,20 @@ test.describe('Profile Page', () => {
     test('update username', async ({page}) => {
         // Go to http://localhost:3000/
         await page.goto('http://localhost:3000/');
+
         // Click text=Sign In
         await page.locator('text=Sign In').click();
-        await expect(page).toHaveURL('http://localhost:3000/signIn');
+        await expect(page).toHaveURL(/.*signIn/);
 
         // Click [placeholder="username"]
         await page.locator('[placeholder="username"]').click();
         // Fill [placeholder="username"]
-        await page.locator('[placeholder="username"]').fill('gtanda');
+        await page.locator('[placeholder="username"]').fill('testuser');
 
         // Click [placeholder="password"]
         await page.locator('[placeholder="password"]').click();
         // Fill [placeholder="password"]
-        await page.locator('[placeholder="password"]').fill('test');
+        await page.locator('[placeholder="password"]').fill('testuser');
 
         // Click button:has-text("Sign In")
         await page.locator('button:has-text("Sign In")').click();
@@ -30,7 +31,7 @@ test.describe('Profile Page', () => {
         // Click [placeholder="enter new username"]
         await page.locator('[placeholder="enter new username"]').click();
         // Fill [placeholder="enter new username"]
-        await page.locator('[placeholder="enter new username"]').fill('gtanda');
+        await page.locator('[placeholder="enter new username"]').fill('gta');
         // Click button:has-text("Update Username")
         await page.locator('button:has-text("Update Username")').click();
 
