87aeb74b55d56b01ab8b228fabbfc2e72e60b1df
gtanda <govind_tanda1@hotmail.com>
2022-09-15 20:32:59 -0700

    added updating email test

--- tests/profileTest.spec.js
+++ tests/profileTest.spec.js
@@ -1,63 +1,85 @@
 // @ts-check
 const {test, expect} = require('@playwright/test');
 
-test.describe('Profile Page', () => {
+test.describe.configure({mode: 'serial'});
+let page;
+test.beforeAll(async ({browser}) => {
+    page = await browser.newPage();
+});
 
-    test.beforeEach(async ({page}) => {
-        // Go to http://localhost:3000/
-        await page.goto('http://localhost:3000/');
+test.afterAll(async () => {
+    // Click text=Logout
+    await page.locator('text=Logout').click();
+    await expect(page).toHaveURL('http://localhost:3000/');
+});
 
-        // Click text=Sign In
-        await page.locator('text=Sign In').click();
-        await expect(page).toHaveURL(/.*signIn/);
+test('update username', async () => {
+    // Go to http://localhost:3000/
+    await page.goto('http://localhost:3000/');
 
-        // Click [placeholder="username"]
-        await page.locator('[placeholder="username"]').click();
-        // Fill [placeholder="username"]
-        await page.locator('[placeholder="username"]').fill('testuser');
+    // Click text=Sign In
+    await page.locator('text=Sign In').click();
+    await expect(page).toHaveURL(/.*signIn/);
 
-        // Click [placeholder="password"]
-        await page.locator('[placeholder="password"]').click();
-        // Fill [placeholder="password"]
-        await page.locator('[placeholder="password"]').fill('testuser');
+    // Click [placeholder="username"]
+    await page.locator('[placeholder="username"]').click();
+    // Fill [placeholder="username"]
+    await page.locator('[placeholder="username"]').fill('testuser');
 
-        // Click button:has-text("Sign In")
-        await page.locator('button:has-text("Sign In")').click();
+    // Click [placeholder="password"]
+    await page.locator('[placeholder="password"]').click();
+    // Fill [placeholder="password"]
+    await page.locator('[placeholder="password"]').fill('testuser');
 
-        // Click text=Profile
-        await page.locator('text=Profile').click();
-        await expect(page).toHaveURL('http://localhost:3000/profile');
-    });
+    // Click button:has-text("Sign In")
+    await page.locator('button:has-text("Sign In")').click();
 
-    test.afterEach(async ({page}) => {
-        // Click text=Logout
-        await page.locator('text=Logout').click();
-        await expect(page).toHaveURL('http://localhost:3000/');
-    });
+    // Click text=Profile
+    await page.locator('text=Profile').click();
+    await expect(page).toHaveURL('http://localhost:3000/profile');
 
-    test('update username', async ({page}) => {
-        // Click [placeholder="enter new username"]
-        await page.locator('[placeholder="enter new username"]').click();
-        // Fill [placeholder="enter new username"]
-        await page.locator('[placeholder="enter new username"]').fill('gta23');
-        // Click button:has-text("Update Username")
-        await page.locator('button:has-text("Update Username")').click();
-        // Welcome text changed to updated username
-        await expect(page.locator('text=Welcome gta23!')).toBeVisible();
 
-        await page.locator('[placeholder="enter new username"]').fill('testuser');
-        // Click button:has-text("Update Username")
-        await page.locator('button:has-text("Update Username")').click();
-    });
+    // Click [placeholder="enter new username"]
+    await page.locator('[placeholder="enter new username"]').click();
+    // Fill [placeholder="enter new username"]
+    await page.locator('[placeholder="enter new username"]').fill('gta23');
+    // Click button:has-text("Update Username")
+    await page.locator('button:has-text("Update Username")').click();
+    // Welcome text changed to updated username
+    await expect(page.locator('text=Welcome gta23!')).toBeVisible();
 
-    test('update email', async ({page}) => {
-        // Click [placeholder="enter new email"]
-        await page.locator('[placeholder="enter new email"]').click();
-        // Fill [placeholder="enter new email"]
-        await page.locator('[placeholder="enter new email"]').fill('updatedemail@emil.ca');
-        // Click button:has-text("Update Email")
-        await page.locator('button:has-text("Update Email")').click();
-        // Click text=Email updated successfully
-        await page.locator('text=Email updated successfully').click();
-    });
+    await page.locator('[placeholder="enter new username"]').fill('testuser');
+    // Click button:has-text("Update Username")
+    await page.locator('button:has-text("Update Username")').click();
+});
+
+test('update email', async () => {
+    // Click [placeholder="enter new email"]
+    await page.locator('[placeholder="enter new email"]').click();
+    // Fill [placeholder="enter new email"]
+    await page.locator('[placeholder="enter new email"]').fill('updatedemail@email.ca');
+    // Click button:has-text("Update Email")
+    await page.locator('button:has-text("Update Email")').click();
+
+    // Click text=Email updated successfully
+    await expect(page.locator('text=Email updated successfully')).toBeVisible();
+});
+
+test('update password', async () => {
+    // Click [placeholder="enter new password"]
+    await page.locator('[placeholder="enter new password"]').click();
+    // Fill [placeholder="enter new password"]
+    await page.locator('[placeholder="enter new password"]').fill('test');
+    // Click button:has-text("Update Password")
+    await page.locator('button:has-text("Update Password")').click();
+
+    // Display text: Password updated successfully
+    await expect(page.locator('text=Password updated successfully')).toBeVisible();
+
+    // Click [placeholder="enter new password"]
+    await page.locator('[placeholder="enter new password"]').click();
+    // Fill [placeholder="enter new password"]
+    await page.locator('[placeholder="enter new password"]').fill('testuser');
+    // Click button:has-text("Update Password")
+    await page.locator('button:has-text("Update Password")').click();
 });
