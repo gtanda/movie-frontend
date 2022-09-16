eee18ce4fdda7d2a9f163617aaa4eac68913b7c0
gtanda <govind_tanda1@hotmail.com>
2022-09-15 20:04:27 -0700

    add email change test

--- tests/profileTest.spec.js
+++ tests/profileTest.spec.js
@@ -2,7 +2,8 @@
 const {test, expect} = require('@playwright/test');
 
 test.describe('Profile Page', () => {
-    test('update username', async ({page}) => {
+
+    test.beforeEach(async ({page}) => {
         // Go to http://localhost:3000/
         await page.goto('http://localhost:3000/');
 
@@ -22,21 +23,41 @@ test.describe('Profile Page', () => {
 
         // Click button:has-text("Sign In")
         await page.locator('button:has-text("Sign In")').click();
-        await expect(page).toHaveURL('http://localhost:3000/');
 
         // Click text=Profile
         await page.locator('text=Profile').click();
         await expect(page).toHaveURL('http://localhost:3000/profile');
+    });
 
+    test.afterEach(async ({page}) => {
+        // Click text=Logout
+        await page.locator('text=Logout').click();
+        await expect(page).toHaveURL('http://localhost:3000/');
+    });
+
+    test('update username', async ({page}) => {
         // Click [placeholder="enter new username"]
         await page.locator('[placeholder="enter new username"]').click();
         // Fill [placeholder="enter new username"]
-        await page.locator('[placeholder="enter new username"]').fill('gta');
+        await page.locator('[placeholder="enter new username"]').fill('gta23');
         // Click button:has-text("Update Username")
         await page.locator('button:has-text("Update Username")').click();
-
         // Welcome text changed to updated username
-        await expect(page.locator('text=Welcome gta!')).toBeVisible();
-    })
+        await expect(page.locator('text=Welcome gta23!')).toBeVisible();
 
-})
+        await page.locator('[placeholder="enter new username"]').fill('testuser');
+        // Click button:has-text("Update Username")
+        await page.locator('button:has-text("Update Username")').click();
+    });
+
+    test('update email', async ({page}) => {
+        // Click [placeholder="enter new email"]
+        await page.locator('[placeholder="enter new email"]').click();
+        // Fill [placeholder="enter new email"]
+        await page.locator('[placeholder="enter new email"]').fill('updatedemail@emil.ca');
+        // Click button:has-text("Update Email")
+        await page.locator('button:has-text("Update Email")').click();
+        // Click text=Email updated successfully
+        await page.locator('text=Email updated successfully').click();
+    });
+});
